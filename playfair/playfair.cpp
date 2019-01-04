7a3b259dffe9825e7bc9b6483812694f86bd9820
Theerapat Muangpoon <chan@Theerapats-MacBook-Pro.local>
2019-01-04 18:20:56 +0000

    update playfair solution

--- playfair/playfair.cpp
+++ playfair/playfair.cpp
@@ -1,94 +1,79 @@
 #include "playfair.h"
-#include <cctype>
-#include <cstring>
-#include <string>
-#include <vector>
 
 using namespace std;
 
-void prepare(const char* input, char* output) {
+void prepare(const string input, char * output) {
+  string str_output;
+  for (auto const& ch: input) {
+    if (isalnum(ch))
+      str_output += toupper(ch);
+  }
+
+  if (str_output.size() % 2 == 1)
+    str_output += 'X';
+  
+  strcpy(output, str_output.c_str());
+}
+
+void grid(const string codeword, char square[6][6]) {
+  string alphanum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  string str_square;
   int count = 0;
-  // convert store alpha numeric to upper in output
-  for (int i = 0; input[i]; i++) {
-    if (isalnum(input[i]))
-      output[count++] = toupper(input[i]);
-  }
-
-  // add 'X' if odd length
-  if (count % 2)
-    output[count++] = 'X';
-
-  // add sentinel
-  output[count] = '\0';
-}
-
-bool occurs_before(const char str[], char letter, int pos) {
-  for (int i = 0; i < pos; i++) {
-    if (str[i] == letter) return true;
-  }
-  return false;
-}
-
-void grid(const char* codeword, char square[6][6]) {
-  string alphanum("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
-  string temp(codeword);
-  int temp_len;
-
-  if (temp.length() % 2) {
-    temp.pop_back();
-  }
-  temp += alphanum;
-  temp_len = temp.length();
-
-  int temp_index = 0, str_len = temp_len;
-  for (int i = 0; i < 36; i++) {
-    while (occurs_before(temp.c_str(), temp[temp_index], temp_index)) {
-      temp_index++;
-      str_len--;
+  for (auto const& ch: codeword) {
+    size_t found = alphanum.find(ch);
+    if (found != string::npos) {
+      square[count / 6][count % 6] = ch;
+      count++;
+      alphanum.erase(alphanum.begin() + found);
     }
-    square[i % 6][i - (i % 6) * 6] = temp[temp_index];
-    temp_index++;
+  }
+
+  for (auto const& ch: alphanum) {
+    square[count / 6][count % 6] = ch;
+    count++;
   }
 }
 
-void bigram(char square[6][6], const char inchar1, const char inchar2, char &outchar1, char &outchar2) {
-  bool isFinished = false;
-  int row1 = -1, col1 = -1, row2 = -1, col2 = -1;
-  for (int i = 0; i < 6 && !isFinished; i++) {
-    for (int j = 0; j < 6 && !isFinished; j++) {
-      if (square[i][j] == inchar1) {
-        row1 = i;
-        col1 = j;
-      } else if (square[i][j] == inchar2) {
-        row2 = i;
-        col2 = j;
-      } 
-      else if (row1 >=0 && col1 >=0 && row2 >=0 && col2 >= 0) {
-        isFinished = true;
+void get_position(const char square[6][6], const char ch, int &row, int &col) {
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 6; j++) {
+      if (square[i][j] == ch) {
+        row = i;
+        col = j;
+        return;
       }
-
     }
   }
+}
+
+void bigram(const char square[6][6], const char inchar1, const char inchar2, char &outchar1, char &outchar2) {
+  int row1, row2, col1, col2;
+
+  get_position(square, inchar1, row1, col1);
+  get_position(square, inchar2, row2, col2);
 
   outchar1 = square[row1][col2];
   outchar2 = square[row2][col1];
 }
 
-void encode(char square[6][6], const char * prepared, char * encoded) {
+void encode(const char square[6][6], const char *prepared, char *encoded) {
   if (!*prepared) {
-    *encoded = '\0';
+    encoded[0] = '\0';
     return;
   }
 
-  bigram(square, *prepared, *(prepared + 1), *encoded, *(encoded + 1));
-  return encode(square, prepared + 2, encoded + 2);
+  bigram(square, prepared[0], prepared[1], encoded[0], encoded[1]);
+
+  encode(square, prepared+2, encoded+2);
 }
 
-void decode(char square[6][6], const char * encoded, char * decoded) {
+void decode(const char square[6][6], const char *encoded, char *decoded) {
   if (!*encoded) {
-    *decoded = '\0';
+    decoded[0] = '\0';
     return;
   }
-  bigram(square, *encoded, *(encoded + 1), *decoded, *(decoded + 1));
-  return encode(square, encoded + 2, decoded + 2);
+
+  bigram(square, encoded[0], encoded[1], decoded[0], decoded[1]);
+
+  decode(square, encoded+2, decoded+2);
 }
--- playfair/playfair.h
+++ playfair/playfair.h
@@ -1,10 +1,14 @@
 #ifndef PLAYFAIR_H
 #define PLAYFAIR_H
 
-void prepare(const char * input, char * output);
-void grid(const char * codeword, char square[6][6]);
-void bigram(char playfair[6][6], const char inchar1, const char inchar2, char &outchar1, char &outchar2);
-void encode(char square[6][6], const char * prepared, char encoded[100]);
-void decode(char square[6][6], const char * encoded, char * decoded);
+#include <cctype>
+#include <string>
+#include <iostream>
+
+void prepare(const std::string input, char * output);
+void grid(const std::string codeword, char square[6][6]);
+void bigram(const char square[6][6], const char inchar1, const char inchar2, char &outchar1, char &outchar2);
+void encode(const char square[6][6], const char *prepared, char *encoded);
+void decode(const char square[6][6], const char *encoded, char *decoded);
 
 #endif
